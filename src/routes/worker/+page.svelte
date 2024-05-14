86c5d7eaf66fc3a5764cb3cd569a95989e6d1571
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-05-15 04:15:49 +0800

    chore(worker): simplify button text

--- src/routes/worker/+page.svelte
+++ src/routes/worker/+page.svelte
@@ -21,10 +21,10 @@
 <button
     type="button"
     class="mb-2 me-2 rounded-lg bg-red-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-    on:click={() => loop(1e8)}>Alert from Main Thread</button
+    on:click={() => loop(1e8)}>Main Thread</button
 >
 <button
     type="button"
     class="mb-2 me-2 rounded-lg bg-green-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 disabled:cursor-not-allowed dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-    on:click={({ currentTarget }) => spawnWorker(currentTarget, 1e8)}>Alert from Worker Thread</button
+    on:click={({ currentTarget }) => spawnWorker(currentTarget, 1e8)}>Worker Thread</button
 >
